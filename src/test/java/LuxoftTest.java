7588b75cf85e16c7b9bd37ca8d08a00fb7654142
Sergey Mezinov <s3rgh@mail.ru>
2021-03-15 19:43:14 +0300

    * Поправлено README * Добавлено два отдельных класса для страниц: LuxoftStartPage и LuxoftCatalogPage * Добавлен пример использование шаблона Chain of Responsibility * Исправлен метод openMenuPoint() * Исправлен метод loadCatalog() * Исправлено метод initialize(): убраны опции веб-драйвера для скачивания, настроен headless режим * Скачивание реализовано через streams * Добавлена возможность использовать браузер многократно без перезапуска * Добавлены константы * Парсинг с pdf в отдельный класс

--- src/test/java/LuxoftTest.java
+++ src/test/java/LuxoftTest.java
@@ -1,23 +1,23 @@
-import org.apache.pdfbox.pdmodel.PDDocument;
-import org.apache.pdfbox.text.PDFTextStripper;
+import helpers.CatalogDownloadHelper;
+import helpers.PDFHelper;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import pages.LuxoftPage;
+import pages.LuxoftCatalogPage;
+import pages.LuxoftStartPage;
 
-import java.io.File;
-import java.io.IOException;
-
+import static constants.Constants.*;
 import static org.assertj.core.api.Assertions.assertThat;
 
 public class LuxoftTest extends TestBase {
 
-    //TODO: добавить README
-    LuxoftPage luxoftPage;
+    LuxoftStartPage luxoftStartPage;
+    LuxoftCatalogPage luxoftCatalogPage;
 
     @BeforeEach
     public void start() {
         initialize();
+        luxoftStartPage = new LuxoftStartPage(webDriver);
     }
 
     @AfterEach
@@ -27,25 +27,13 @@ public class LuxoftTest extends TestBase {
 
     @Test
     public void checkRequiredCourseExists() throws InterruptedException {
-        luxoftPage = new LuxoftPage(webDriver);
-        //TODO: строку - адрес сайта перенести в константы
-        luxoftPage.openUrl("https://www.luxoft-training.ru");
-        luxoftPage.isPageInitialized();
-        luxoftPage.openMenuPoint();
-        luxoftPage.loadCatalog();
-        Thread.sleep(5000);
 
-        //TODO: вынести работу с pdf в отдельный класс, вызывать необходимый метод в этом месте
-        String text = "";
-        File pdfFile = new File(getCurrentWorkingDirectory() + "\\luxoft_training_catalog.pdf");
-        try {
-            PDDocument pdDocument = PDDocument.load(pdfFile);
-            PDFTextStripper pdfTextStripper = new PDFTextStripper();
-            text = pdfTextStripper.getText(pdDocument);
-            pdfFile.delete();
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-        assertThat(text.contains("Шаблоны проектирования приложений масштаба предприятия")).isEqualTo(true);
+        luxoftCatalogPage = luxoftStartPage
+                .openUrl(BASE_URL)
+                .openMenuPoint(MENU_CATALOG);
+
+        CatalogDownloadHelper.downloadFile(luxoftCatalogPage.loadCatalog(DOWNLOAD_CATALOG));
+        Thread.sleep(1000);
+        assertThat(PDFHelper.checkRequiredTextInPdf(SOURCE_DIRECTORY)).isEqualTo(true);
     }
 }
